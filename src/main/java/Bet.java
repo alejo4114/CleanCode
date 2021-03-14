e01b0afc67ce788dcad7c1f4aa33edb6c6292062
Alejandro <Alejandro@DESKTOP-BOGJDLM>
2021-03-13 23:18:34 -0500

    Primer borrador de estructura en la clase Roulette y creacion de clase Bet

---
+++ src/main/java/Bet.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Bet {
+    private int number;
+    private String color;
+    private int amount;
+
+    public Bet() {
+    }
+
+    public Bet(int number, String color, int amount) {
+        this.number = number;
+        this.color = color;
+        this.amount = amount;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+        
+}
--- src/main/java/Roulette.java
+++ src/main/java/Roulette.java
@@ -9,5 +9,51 @@
  * @author Alejandro
  */
 public class Roulette {
-    
+
+    private int id;
+    private int[] numbers = new int[36];
+    private String[] colors = {"Rojo", "Negro"};
+
+    public Roulette() {
+    }
+
+    public Roulette(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public String[] getColors() {
+        return colors;
+    }
+
+    public void setColors(String[] colors) {
+        this.colors = colors;
+    }
+
+    public int CreateRoulette() {
+        id = id+1;
+
+        return id;
+    }
+
+    public String OpenRoulette(int id) {
+        String bet = null;
+
+        return bet;
+    }
 }
