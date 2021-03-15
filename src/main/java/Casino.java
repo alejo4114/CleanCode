8c660d38ce22b7f15a80a18e68cdc1aeb6f5a7b4
Alejandro <Alejandro@DESKTOP-BOGJDLM>
2021-03-15 00:04:22 -0500

    Creacion de metodos en clases y principales funciones dentro del main, ya crea, abre y lista ruletas

--- src/main/java/Casino.java
+++ src/main/java/Casino.java
@@ -1,9 +1,11 @@
+
+import java.util.*;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Alejandro
@@ -14,7 +16,33 @@ public class Casino {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Roulette rou = new Roulette();
+        Scanner sc = new Scanner(System.in);    
+        boolean close=false;
+        do {
+            System.out.println("1. crear ruleta, 2. abrir ruleta, 3. listar ruletas, 4. Salir");
+            int sel = sc.nextInt();
+            switch (sel) {
+                case 1:
+                    System.out.println(rou.CreateRoulette());
+                    break;
+                    
+                case 2:
+                    System.out.println("Que ruleta desea abrir");
+                    int r = sc.nextInt();
+                    rou.OpenRoulette(r);
+                    break;
+                    
+                case 3:
+                    rou.ListRoulettes();
+                    break;
+                    
+                case 4: 
+                    close=true;
+                    break;
+            }
+        } while (close != true);
         // TODO code application logic here
     }
-    
+
 }
--- src/main/java/Roulette.java
+++ src/main/java/Roulette.java
@@ -1,9 +1,11 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Alejandro
@@ -12,9 +14,14 @@ public class Roulette {
 
     private int id;
     private int[] numbers = new int[36];
-    private boolean estado;
+    private boolean condition;
+    ArrayList<Roulette> roulettes = new ArrayList();
 
     public Roulette() {
+    }
+
+    public Roulette(int id) {
+        this.id = id;
     }
 
     public int getId() {
@@ -33,25 +40,36 @@ public class Roulette {
         this.numbers = numbers;
     }
 
-    public boolean isEstado() {
-        return estado;
+    public boolean isCondition() {
+        return condition;
     }
 
-    public void setEstado(boolean estado) {
-        this.estado = estado;
+    public void setCondition(boolean condition) {
+        this.condition = condition;
     }
 
-  
     public int CreateRoulette() {
-        id = id+1;
-        this.numbers=numbers;
+        id = id + 1;
+        Roulette rou = new Roulette(id = id);
+        roulettes.add(rou);
 
         return id;
     }
 
     public String OpenRoulette(int id) {
-        String bet = null;
+        for (int i = 0; i < roulettes.size(); i++) {
+            if (roulettes.get(i).id == id && roulettes.get(i).condition == false) {
+                roulettes.get(i).condition=true;
+                System.out.println("Operacion Exitosa, mesa abierta");
+            }
+        }
+        return null;
+    }
 
-        return bet;
+    public void ListRoulettes() {
+        for (int i = 0; i < roulettes.size(); i++) {
+            System.out.println(roulettes.get(i).id);
+            System.out.println(roulettes.get(i).condition);
+        }
     }
 }
